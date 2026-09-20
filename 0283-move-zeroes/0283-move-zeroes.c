b1e4be4856df113fd94933477bd5d3d83b1c5d9e
RAJASUDHAMARIMUTHU <rajakirubha2007@gmail.com>
2026-09-20 18:19:04 +0530

    Time: 0 ms (100%), Space: 20.4 MB (9.31%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -0,0 +1,15 @@
+void moveZeroes(int* nums, int numsSize) {
+    int j=0;
+    int temp;
+    for(int i=0;i<numsSize;i++){
+          if(nums[i]!=0){
+            temp=nums[j];
+            nums[j]=nums[i];
+            nums[i]=temp;
+            j++;
+            
+          }
+    }
+         
+    
+}
